83a380a98d9e14c04bed8c1f1ee09e596fc566e0
ImJAraya <fabiarayamontero@gmail.com>
2025-04-28 20:52:38 +0000

    /fix- backpopulate en modelo entrada

--- src/api/models.py
+++ src/api/models.py
@@ -21,7 +21,7 @@ class User(db.Model):
         }
     
 class Entrada(db.Model):
-    __tablename__ = 'Entrada'
+    __tablename__ = 'entrada'
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -29,7 +29,7 @@ class Entrada(db.Model):
     entry_text = db.Column(db.String(1000), nullable=True)
     date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
-    user = db.relationship('User', back_populates='Entrada')
+    user = db.relationship('User', back_populates='diary_entries')
 
     def serialize(self):
         return {
