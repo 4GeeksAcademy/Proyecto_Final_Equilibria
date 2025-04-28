db0d5dfa1a5f05b89ae30c33572746e2656fc381
ImJAraya <fabiarayamontero@gmail.com>
2025-04-28 19:16:08 +0000

    /feat- model Entrada

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy  # Para base de datos
-
+import datetime
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -9,8 +9,8 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+    diary_entries = db.relationship('DiaryEntry', back_populates='user', lazy=True)
+    favorite_quotes = db.relationship('FavoriteQuote', back_populates='user', lazy=True)
 
     def serialize(self):
         return {
@@ -18,4 +18,24 @@ class User(db.Model):
             "email": self.email,
             "name": self.name,
             # do not serialize the password, its a security breach
+        }
+    
+class DiaryEntry(db.Model):
+    __tablename__ = 'diary_entry'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    mood_tag = db.Column(db.String(50), nullable=False)
+    entry_text = db.Column(db.Text, nullable=True)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    user = db.relationship('User', back_populates='diary_entries')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "mood_tag": self.mood_tag,
+            "entry_text": self.entry_text,
+            "date": self.date.isoformat()
         }
