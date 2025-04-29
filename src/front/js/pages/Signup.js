72ded3a45b6a65718694858d3386187e94fe28b9
Alexander Morales <alexandermorales.n1@gmail.com>
2025-04-29 10:22:13 +0000

    add signup form

---
+++ src/front/js/pages/Signup.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Signup = () => {
+    return (
+        <div>
+           <form className="px-4">
+                <div className="mb-3">
+                    <label htmlFor="inputName" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="inputName" aria-describedby="nameHelp"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
