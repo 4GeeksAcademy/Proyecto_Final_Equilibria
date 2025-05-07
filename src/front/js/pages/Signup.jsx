4466c5f12251644743092c52cad80e185d83f67c
Alexander Morales <alexandermorales.n1@gmail.com>
2025-05-07 18:08:43 +0000

    add return button in signup form

--- src/front/js/pages/Signup.jsx
+++ src/front/js/pages/Signup.jsx
@@ -61,28 +61,32 @@ export const Signup = () => {
         <div>
             <form className="px-4" ref={signupFormRef}>
                 <div className="mb-3">
-                    <label htmlFor="inputName" className="form-label">Name</label>
+                    <label htmlFor="inputName" className="form-label">Nombre</label>
                     <input onChange={handleName} type="text" className="form-control" id="inputName" aria-describedby="nameHelp" />
                 </div>
                 <div className="form-check form-check-inline">
                     <input onChange={handleGender} className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="male"/>
-                        <label className="form-check-label" htmlFor="inlineRadio1">Male</label>
+                        <label className="form-check-label" htmlFor="inlineRadio1">Masculino</label>
                 </div>
                 <div className="form-check form-check-inline">
                     <input onChange={handleGender} className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="female" />
-                        <label className="form-check-label" htmlFor="inlineRadio2">Female</label>
+                        <label className="form-check-label" htmlFor="inlineRadio2">Femenino</label>
                 </div>
                 
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
                     <input onChange={handleEmail} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <div id="emailHelp" className="form-text">No compartiremos tu email con nadie más.</div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
                     <input onChange={handlePassword} type="password" className="form-control" id="exampleInputPassword1" />
                 </div>
-                <button onClick={handleSubmit} className="btn btn-primary">Submit</button>
+                <div>
+                    <button onClick={handleSubmit} className="btn btn-primary mx-1">Enviar</button>
+                    <button onClick={()=> navigate('/')} className="btn btn-primary mx-1">Regresar</button>
+                </div>
+
             </form>
         </div>
     )
