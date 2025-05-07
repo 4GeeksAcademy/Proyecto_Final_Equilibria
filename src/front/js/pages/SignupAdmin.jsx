95dfce2024ba67c180cb6a8d14e825052836cb09
Alexander Morales <alexandermorales.n1@gmail.com>
2025-05-07 18:45:04 +0000

    fix signup forms

--- src/front/js/pages/SignupAdmin.jsx
+++ src/front/js/pages/SignupAdmin.jsx
@@ -59,30 +59,26 @@ const SignupAdmin = () => {
         <div>
             <form className="px-4" ref={signupFormRef}>
                 <div className="mb-3">
-                    <label htmlFor="inputName" className="form-label">Name</label>
+                    <label htmlFor="inputName" className="form-label">Nombre</label>
                     <input onChange={handleName} type="text" className="form-control" id="inputName" aria-describedby="nameHelp" />
                 </div>
                 <div className="form-check form-check-inline">
                     <input onChange={handleGender} className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="male" />
-                    <label className="form-check-label" htmlFor="inlineRadio1">Male</label>
+                    <label className="form-check-label" htmlFor="inlineRadio1">Masculino</label>
                 </div>
                 <div className="form-check form-check-inline">
                     <input onChange={handleGender} className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="female" />
-                    <label className="form-check-label" htmlFor="inlineRadio2">Female</label>
+                    <label className="form-check-label" htmlFor="inlineRadio2">Femenino</label>
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
                     <input onChange={handleEmail} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <div id="emailHelp" className="form-text">No compartiremos tu email con nadie</div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
                     <input onChange={handlePassword} type="password" className="form-control" id="exampleInputPassword1" />
-                </div>
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptionAdmin" id="inlineRadio3" value="admin" checked readOnly/>
-                    <label className="form-check-label" htmlFor="inlineRadio3" >Admin</label>
                 </div>
                 <div className="py-3">
                     <button onClick={handleSubmit} className="btn btn-primary mx-2">Enviar</button>
